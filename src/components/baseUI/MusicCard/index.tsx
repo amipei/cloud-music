62c9839b4b04e0e3c1f3a019419a86a3016c3f43
amipei <156516399@qq.com>
2020-02-19 20:43:23 +0800

    新增 music-card组件

---
+++ src/components/baseUI/MusicCard/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import LazyLoad from "react-lazyload";
+import { FiPlay } from "react-icons/fi";
+import './music_card.scss'
+import { disposePlayCount } from '../../../utils';
+interface MusicCardProps {
+  style?: React.CSSProperties
+  coverImg: string
+  coverAlt?: string
+  playCount?: number
+  name: string
+  handleClick?: () => void
+}
+
+const renderCover = (prefixCls: string, props: MusicCardProps) => {
+  const { style, coverImg, coverAlt, playCount = -1} = props
+
+  const lazyloadHeight = style ? style.width ? parseInt(style.width as string) : undefined : undefined
+  return (
+    <div className={`${prefixCls}__cover`} >
+      <div className={`${prefixCls}__decorate`}></div>
+      {
+        playCount >= 0
+          ? (
+            <span className={`${prefixCls}__play-count`}>
+              <FiPlay color="white" size="1em" />
+              {disposePlayCount(playCount)}
+            </span>
+          )
+          : null
+      }
+      <LazyLoad height={lazyloadHeight}>
+        <img src={coverImg} alt={coverAlt} />
+      </LazyLoad>
+    </div>
+  )
+}
+
+const renderTitle = (prefixCls: string, props: MusicCardProps) => {
+  const { name } = props
+  return (
+    <div className={`${prefixCls}__title`}>{ name }</div>
+  )
+}
+
+const MusicCard: React.FC<MusicCardProps> = (props) => {
+  const { style } = props
+  const { handleClick } = props
+
+  const prefixCls = 'music-card'
+  return (
+    <div style={style} className={prefixCls} onClick={handleClick}>
+      {renderCover(prefixCls, props)}
+      {renderTitle(prefixCls, props)}
+    </div>
+  )
+}
+
+export default MusicCard
